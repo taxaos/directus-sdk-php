f52daa00b489c1b1b905964d0a1589eb951c1431
Welling Guzman <hola@wellingguzman.com>
2016-11-17 06:36:42 -0400

    Add phpdoc

--- src/ClientRemote.php
+++ src/ClientRemote.php
@@ -21,101 +21,161 @@ class ClientRemote extends BaseClientRemote
     protected $baseEndpoint = 'http://localhost/api';
     protected $hostedBaseEndpointFormat = 'https://%s.directus.io/api';
 
+    /**
+     * @inheritdoc
+     */
     public function getTables(array $params = [])
     {
         return $this->performRequest('GET', static::TABLE_LIST_ENDPOINT);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getTable($tableName)
     {
         return $this->performRequest('GET', static::TABLE_INFORMATION_ENDPOINT, $tableName);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getColumns($tableName, array $params = [])
     {
         return $this->performRequest('GET', static::COLUMN_LIST_ENDPOINT, $tableName);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getColumn($tableName, $columnName)
     {
         return $this->performRequest('GET', static::COLUMN_INFORMATION_ENDPOINT, [$tableName, $columnName]);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getEntries($tableName, array $options = [])
     {
         return $this->performRequest('GET', static::TABLE_ENTRIES_ENDPOINT, $tableName, null, $options);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getEntry($tableName, $id, array $options = [])
     {
         return $this->performRequest('GET', static::TABLE_ENTRY_ENDPOINT, [$tableName, $id]);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getUsers(array $params = [])
     {
         return $this->getEntries('directus_users', $params);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getUser($id, array $params = [])
     {
         return $this->getEntry($id, 'directus_users', $params);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getGroups()
     {
         return $this->performRequest('GET', static::GROUP_LIST_ENDPOINT);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getGroup($groupID)
     {
         return $this->performRequest('GET', static::GROUP_INFORMATION_ENDPOINT, $groupID);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getGroupPrivileges($groupID)
     {
         return $this->performRequest('GET', static::GROUP_PRIVILEGES_ENDPOINT, $groupID);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getFiles()
     {
         return $this->performRequest('GET', static::FILE_LIST_ENDPOINT);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getFile($fileID)
     {
         return $this->performRequest('GET', static::FILE_INFORMATION_ENDPOINT, $fileID);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getSettings()
     {
         return $this->performRequest('GET', static::SETTING_LIST_ENDPOINT);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getSettingsByCollection($collectionName)
     {
         return $this->performRequest('GET', static::SETTING_COLLECTION_ENDPOINT, $collectionName);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getMessages($userId)
     {
         return $this->performRequest('GET', static::MESSAGES_USER_ENDPOINT, $userId);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function createEntry($tableName, array $data)
     {
         return $this->performRequest('POST', static::TABLE_ENTRY_CREATE_ENDPOINT, $tableName, $data);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function updateEntry($tableName, $id, array $data)
     {
         return $this->performRequest('PUT', static::TABLE_ENTRY_UPDATE_ENDPOINT, [$tableName, $id], $data);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function deleteEntry($tableName, $ids)
     {
         return $this->performRequest('DELETE', static::TABLE_ENTRY_DELETE_ENDPOINT, [$tableName, $ids]);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function createUser(array $data)
     {
         // @TODO: Add hooks
@@ -127,6 +187,9 @@ class ClientRemote extends BaseClientRemote
         return $this->createEntry('directus_users', $data);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function updateUser($id, array $data)
     {
         // @TODO: Add hooks
@@ -138,21 +201,33 @@ class ClientRemote extends BaseClientRemote
         return $this->updateEntry('directus_users', $id, $data);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function deleteUser($ids)
     {
         return $this->deleteEntry('directus_users', $ids);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function createFile(array $data)
     {
         return $this->createEntry('directus_files', $data);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function updateFile($id, array $data)
     {
         return $this->updateEntry('directus_files', $id, $data);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function deleteFile($ids)
     {
         return $this->deleteEntry('directus_files', $ids);
